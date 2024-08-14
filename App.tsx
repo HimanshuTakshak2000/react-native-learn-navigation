4b3491fa58a2b552f4c052474f9bb7fbd58c8b2e
HimanshuTakshak2000 <himanhutakshak2000@gmail.com>
2024-08-14 12:52:09 +0530

    react navigation start

--- App.tsx
+++ App.tsx
@@ -6,113 +6,108 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Stack = createNativeStackNavigator();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+function App(): React.JSX.Element {
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
+    <NavigationContainer>
+
+      <Stack.Navigator initialRouteName='Home' screenOptions={
+        {
+          headerStyle:{
+            backgroundColor:'brown',
+            
           },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
+
+          headerTitleStyle:{
+            fontSize:20, 
+            
           },
-        ]}>
-        {children}
-      </Text>
+          headerTitleAlign:'center',
+          headerTintColor:'skyblue',
+          
+        }
+      }>
+        <Stack.Screen name='Home' component={Home} options={
+
+          {
+            title:"Login",
+            headerStyle:{
+              backgroundColor:'skyblue',
+                      
+            },
+          
+            headerTitleStyle:{
+              fontSize:25, 
+                      
+            },
+            headerTitleAlign:'left',
+            headerTintColor:'red',
+                    
+          }
+        } />
+        <Stack.Screen name='Detail' component={DetailsScreen} />
+      </Stack.Navigator>
+
+    </NavigationContainer>
+  );
+}
+
+const Home = ()=>{
+  return(
+    <View style={style.homeContainer}>
+      <Text style ={style.homeContainerText}>Home Screen</Text>
+    </View>
+
+  )
+}
+
+function DetailsScreen(){
+  return(
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'gray' }}>
+      <Text style={{fontSize:30}}>Details Screen</Text>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const style = StyleSheet.create({
+  homeContainer:{ 
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    backgroundColor:'gray' 
+  },
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  homeContainerText:{
+    fontSize:25,
+    color:'orange',
+  }
+})
 
 export default App;
+
+/*
+  Note :- 1) The component prop accepts component, not a render function. Don't pass an inline function (e.g. component={() => <HomeScreen />}), or your component will unmount and remount losing all state when the parent component re-renders.
+
+  2) React Native doesn't have a built-in API for navigation like a web browser does. React Navigation provides this for you, along with the iOS and Android gestures and animations to transition between screens.
+
+  3) Stack.Navigator is a component that takes route configuration as its children with additional props for configuration and renders our content.
+
+  4) Each Stack.Screen component takes a name prop which refers to the name of the route and component prop which specifies the component to render for the route. These are the 2 required props.
+
+  5) To specify what the initial route in a stack is, provide an initialRouteName as the prop for the navigator.
+
+  6) To specify screen-specific options, we can pass an options prop to Stack.Screen and for common options, we can pass screenOptions to Stack.Navigator.
+
+*/
